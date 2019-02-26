5ba13627c158d0e095d918222320ac6d8d7fd9e6
Martin Benjamins <ictmartin@gmail.com>
2019-02-26 15:45:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WoW.tools - Soon™</title>
+	<style type='text/css'>
+		html{
+			background-color: #2c2c2c;
+		}
+
+		#message{
+			width: 100%;
+			text-align: center;
+			color: white;
+			font-family: "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+		}
+
+		a{
+			text-decoration: none;
+			color: white;
+			font-size: 12px;
+		}
+	</style>
+</head>
+<body>
+	<div id='message'>
+		Coming soon™!
+		<br>
+		<a href='//twitter.com/Marlamin'><i>- Marlamin</i></a>
+	</div>
+</body>
+</html>
