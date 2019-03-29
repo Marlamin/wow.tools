98148c454c600c9d4203446892cb216029fb3220
Martin Benjamins <ictmartin@gmail.com>
2019-03-29 12:26:10 +0100

    Update favicon

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 	<title>WoW.tools</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<link rel="icon" type="image/png" href="/img/cog.png" />
+	<link rel="icon" type="image/png" href="/img/cogw.png" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<link href="/css/style.css?v=<?=filemtime("/var/www/wow.tools/css/style.css")?>" rel="stylesheet">
