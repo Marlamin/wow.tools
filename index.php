c2d3600aec4d489ea2eeb9ef7f20793794c4d497
Martin Benjamins <ictmartin@gmail.com>
2019-04-07 20:31:48 +0200

    Remove index placeholder

--- index.php
+++ index.php
@@ -1,50 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>WoW.tools - Soon™</title>
-	<link href="/css/style.css?v=<?=filemtime("/var/www/wow.tools/css/style.css")?>" rel="stylesheet">
-	<style type='text/css'>
-		html{
-			background-color: #343a40 !important;
-		}
-
-		#message{
-			width: 100%;
-			text-align: center;
-			color: white;
-			font-family: "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-			padding-top: 1rem;
-		}
-
-		a{
-			text-decoration: none;
-			color: white;
-			font-size: 12px;
-		}
-
-		#logo{
-			position: relative;
-			color: white;
-			font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-			font-weight: 400;
-			width: 150px;
-			margin: auto;
-		}
-
-		#logo #cog{
-			left: 29px;
-		}
-	</style>
-</head>
-<body>
-	<div id='message'>
-		<div id='logo'>
-			<div id='cog'>&nbsp;</div>
-			<div id='nocog'><img src='/img/w.svg'><img src='/img/w.svg'><span>.Tools</span></div>
-		</div>
-		Coming soon™!
-		<br>
-		<a href='//twitter.com/Marlamin'><i>- Marlamin</i></a>
-	</div>
-</body>
-</html>
+<? require_once("inc/header.php"); ?>
+<div class='container-fluid'>
+<h3>Welcome to WoW.tools!</h3>
+<p>
+The site is not ready for prime-time use just yet and is not actively kept up-to-date with new WoW builds as I am still gathering design/UX feedback. Please use <a href='//bnet.marlam.in'>bnet.marlam.in</a> for now if you intend on actually using any of the tools on this site.
+</p>
+</div>
+<? require_once("inc/footer.php"); ?>
