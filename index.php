6f3654b738750868d8b1dda377b8711932c353ff
Martin Benjamins <ictmartin@gmail.com>
2019-04-26 11:45:03 +0200

    Update index contents

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class='container-fluid'>
 <h3>Welcome to WoW.tools!</h3>
 <p>
-While many features are still being ported over from <a href='//bnet.marlam.in'>bnet.marlam.in</a>, most of the features on this site should now be in a usable state. If there are any issues please let me know!
+Having had many different sites for many of the tools I work on, I decided to move them all under one roof. This is what WoW.tools is, a collection for all those tools under one roof, allowing for better integration between the tools and costing me less time to keep everything up-to-date separately. Keep in mind many of the tools are still only meant for the technical/datamining minded amongst you, some stuff might not be friendly to beginners at this stage. I plan on adding tutorials/guides on how to get started doing some basic stuff.
 </p>
 </div>
 <? require_once("inc/footer.php"); ?>
