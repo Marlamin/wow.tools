5df155932b3e90187a6aa3a3f643a5c9b5cf09bd
Martin Benjamins <marlamin@marlamin.com>
2019-05-04 02:19:14 +0200

    WIP user filename submission queue

---
+++ files/submitFiles.php
@@ -0,0 +1,107 @@
+<? require_once("../inc/header.php");
+
+$filelimit = 20000;
+
+$kfq = $pdo->query("SELECT id, filename FROM wow_rootfiles WHERE verified = 0")->fetchAll();
+foreach($kfq as $row){
+	$knownfiles[$row['id']] = $row['filename'];
+}
+
+$uq = $pdo->prepare("UPDATE wow_rootfiles SET filename = ? WHERE id = ?");
+
+if(!empty($_SESSION['loggedin'])){
+	if(!empty($_POST['files'])){
+
+		$files = explode("\n", $_POST['files']);
+
+		if(count($files) > $filelimit){
+			die("There currently is a limit of <b>".$filelimit." files</b> per request. You entered ".count($files)." files.");
+		}
+
+		$log = [];
+		$suggestedfiles = [];
+
+		foreach($files as $file){
+			if(empty($file))
+				continue;
+
+			$split = explode(";", $file);
+			$fdid = (int)$split[0];
+			if(count($split) != 2)
+				continue;
+			$fname = strtolower(str_replace("\\", "/", trim($split[1])));
+
+			if(array_key_exists($fdid, $knownfiles)){
+				if(empty($knownfiles[$fdid])){
+					// No filename currently set
+					$log[] = "Adding <kbd>".$fname."</kbd> to ".$fdid;
+					$suggestedfiles[$fdid] = $fname;
+				}else if($knownfiles[$fdid] != $fname){
+					// Submitted filename differs from current filename
+					$log[] = "Overriding <kbd>".$knownfiles[$fdid]."</kbd> (".$fdid.") with <kbd>".$fname."</kbd>";
+					$suggestedfiles[$fdid] = $fname;
+				}else{
+					// Submitted filename is the same
+					$log[] = "Skipping <kbd>".$fname."</kbd>, same as <kbd>".$knownfiles[$fdid]."</kbd> (".$fdid.")";
+				}
+			}else{
+				// File does not exist
+				$log[] = "<b>WARNING!</b> FileDataID " . $fdid . " does not exist or is a file with a bruteforcable lookup!";
+			}
+		}
+
+		$isq = $pdo->prepare("INSERT INTO wow_rootfiles_suggestions (filedataid, filename, userid, submitted) VALUES (?, ?, ?, ?)");
+
+		if(empty($_POST['checkBox'])){
+			if(isset($_SESSION['rank']) && $_SESSION['rank'] > 0){
+				// Send to wow_rootfiles
+			}else{
+				// Send to queue
+
+				// Set insert time to one value in case in case things take longer than a second to insert
+				$time = date("Y-m-d H:i:s");
+
+				foreach($suggestedfiles as $fdid => $fname){
+					$isq->execute([$fdid, $fname, $_SESSION['userid'], $time]);
+				}
+			}
+		}else{
+			echo "<div class='alert alert-warning'><b>Warning</b> Currently only comparing with listfile, not saving anything to database.</div>";
+		}
+
+		$wroteSomething = false;
+
+		if($wroteSomething){
+			flushQueryCache();
+		}
+
+		echo "<div class='container-fluid'>";
+		echo "<h4>Log</h4>";
+		echo "<pre style='color: var(--text-color); max-height: 500px; overflow-y: scroll'>";
+		echo implode("\n", $log);
+		echo "</pre>";
+		echo "</div>";
+	}
+}
+
+?>
+<div class="container-fluid">
+<?php if(empty($_SESSION['loggedin'])){?>
+	<div class='alert alert-danger'>
+		You need to be logged in to submit filenames.
+	</div>
+<? }else{ ?>
+	<p>Enter files in the textbox below to suggest filenames for the community listfile. Each line must start with a filedataid, followed by the <kbd>;</kbd> and then the suggested filename.<br><b>Please note:</b> All submitted files will have to be checked by a moderator before being added to the listfile to prevent purposefully incorrect filenames being added to the system.</p>
+	<p>Formatting example:<br><kbd>2961114;world/expansion07/doodads/dungeon/doodads/8du_mechagon_anvil01.m2</kbd><br><kbd>2961119;world/expansion07/doodads/dungeon/doodads/8du_mechagon_anvil0100.skin</kbd></p>
+	<div class='alert alert-warning'>A maximum of <b><?=$filelimit?> files</b> per request is allowed.</div>
+	<form method='post' action='submitFiles.php'>
+		<input id='checkBox' type='checkbox' name='write'> <label for='checkBox'>Do not submit to queue and just compare with current listfile ("dry-run")</label>
+		<br>
+		<textarea name='files' rows='15' cols='200'></textarea>
+		<br>
+		<input class='btn btn-success' type='submit' value='Submit'>
+	</form>
+<? } ?>
+</div>
+
+<? include("../inc/footer.php"); ?>
