48622d0dbc97468a8ca433d0433c3b4cf1e36d90
Martin Benjamins <ictmartin@gmail.com>
2019-05-10 11:36:37 +0200

    Show message when nothing is waiting for approval

--- files/submitQueue.php
+++ files/submitQueue.php
@@ -73,33 +73,41 @@ if(!empty($_SESSION['loggedin']) && $_SESSION['rank'] > 0){
 		</div>
 	<? }else{ ?>
 		<table class='table table-striped table-condensed'>
-			<thead><tr><th style='width: 100px'>User</th><th style='width: 200px'>Submitted at</th><th>Files</th><th style='width: 100px'>&nbsp;</th><th style='width: 100px'>&nbsp;</th></tr></thead>
 			<?php
 			$previousTime = '';
 			$cq = $pdo->prepare("SELECT filename FROM wow_rootfiles WHERE id = ?");
 			$suggestions = $pdo->query("SELECT * FROM wow_rootfiles_suggestions WHERE status = 'todo'")->fetchAll();
-			foreach($suggestions as $row){
-				if($previousTime != $row['submitted']){
-					if($previousTime == ''){
-						$endTag = "</table></pre></td><td><a href='?approve=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
+			if(count($suggestions == 0)){
+				echo "There are currently no submitted files waiting for approval.";
+			}else{
+				echo "<thead><tr><th style='width: 100px'>User</th><th style='width: 200px'>Submitted at</th><th>Files</th><th style='width: 100px'>&nbsp;</th><th style='width: 100px'>&nbsp;</th></tr></thead>";
+				foreach($suggestions as $row){
+					if($previousTime != $row['submitted']){
+						if($previousTime == ''){
+							$endTag = "</table></pre></td><td><a href='?approve=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
 
-					}else{
-						$endTag = "</table></pre></td><td><a href='?approve=".urlencode($previousTime)."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($previousTime)."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
+						}else{
+							$endTag = "</table></pre></td><td><a href='?approve=".urlencode($previousTime)."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($previousTime)."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
+						}
+						if($previousTime != '') echo $endTag;
+						echo "<tr><td>".getUsernameByUserID($row['userid'])."</td><td>".$row['submitted']."</td><td><pre style='max-height: 200px; overflow-y: scroll; color: var(--text-color)'><table class='table table-minimal'><thead style='position: sticky; top: 0px;'><tr><th>FileDataID</th><th>Suggested name</th><th>Current name (if set)</th></tr></thead>";
 					}
-					if($previousTime != '') echo $endTag;
-					echo "<tr><td>".getUsernameByUserID($row['userid'])."</td><td>".$row['submitted']."</td><td><pre style='max-height: 200px; overflow-y: scroll; color: var(--text-color)'><table class='table table-minimal'><thead style='position: sticky; top: 0px;'><tr><th>FileDataID</th><th>Suggested name</th><th>Current name (if set)</th></tr></thead>";
+
+					echo "<tr><td>".$row['filedataid']."</td><td>".$row['filename']."</td>";
+					$cq->execute([$row['filedataid']]);
+					$cr = $cq->fetch();
+					if(!empty($cr['filename'])){
+						echo "<td>".$cr['filename']."</td>";
+					}
+					echo "</tr>";
+					$previousTime = $row['submitted'];
 				}
 
-				echo "<tr><td>".$row['filedataid']."</td><td>".$row['filename']."</td>";
-				$cq->execute([$row['filedataid']]);
-				$cr = $cq->fetch();
-				if(!empty($cr['filename'])){
-					echo "<td>".$cr['filename']."</td>";
+				if($previousTime != ''){
+					echo "</table></pre></td><td><a href='?approve=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
 				}
-				echo "</tr>";
-				$previousTime = $row['submitted'];
 			}
-			echo "</table></pre></td><td><a href='?approve=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-success'>Approve</a></td><td><a href='?decline=".urlencode($row['submitted'])."' class='btn btn-sm btn-outline-danger'>Decline</a></td></tr>";
+
 			?>
 		</table>
 	<? } ?>
