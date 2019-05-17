3217b23dbdc9a0ba94dab79e20ae3379d1665b74
Martin Benjamins <ictmartin@gmail.com>
2019-05-17 10:31:15 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<? 
+http_response_code(404);
+require_once("inc/header.php"); 
+?>
+<div class='container-fluid'>
+	<h3>404 page not found</h3>
+	<p>
+		Sorry, nothing here!
+	</p>
+</div>
+<? require_once("inc/footer.php"); ?>
