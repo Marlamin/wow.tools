7ea540d4d66900fb1758c3ba58988b11c1e57105
Martin Benjamins <marlamin@marlamin.com>
2019-05-18 13:35:25 +0200

    Remove dev URLs

--- files/diff.php
+++ files/diff.php
@@ -5,13 +5,13 @@ if (empty($_GET['from']) || empty($_GET['to']) || empty($_GET['filedataid'])) {
     die("Not enough information!");
 }
 
-$diff_api_url = "http://wow.tools.local:8080/files/scripts/diff_api.php?from=" . $_GET['from'] . "&to=" . $_GET['to'] . "&filedataid=" . $_GET['filedataid'];
+$diff_api_url = "/files/scripts/diff_api.php?from=" . $_GET['from'] . "&to=" . $_GET['to'] . "&filedataid=" . $_GET['filedataid'];
 
 ?>
-<link rel="stylesheet" type="text/css" href="http://wow.tools.local:8080/css/diff2html.min.css">
+<link rel="stylesheet" type="text/css" href="/css/diff2html.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://wow.tools.local:8080/js/diff2html-ui.min.js"></script>
-<script type="text/javascript" src="http://wow.tools.local:8080/js/diff2html.min.js"></script>
+<script type="text/javascript" src="/js/diff2html-ui.min.js"></script>
+<script type="text/javascript" src="/js/diff2html.min.js"></script>
 
 <script type="text/javascript" charset="utf-8">
     $(document).ready(function() {
