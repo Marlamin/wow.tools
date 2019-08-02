4b253ee87a6fc9962b09589251acc30a51839a0c
Martin Benjamins <marlamin@marlamin.com>
2019-08-03 01:47:21 +0200

    Add CreatureModelData importing

--- builds/scripts/updateGameData.php
+++ builds/scripts/updateGameData.php
@@ -68,3 +68,22 @@ if(!file_exists($csv)){
 	");
 	echo "..done!\n";
 }
+
+$db2 = "https://wow.tools/api/export/?name=creaturemodeldata&build=".$outdir;
+$csv = "/tmp/creaturemodeldata.csv";
+if(file_exists($csv)){ unlink($csv); }
+$outputdump = shell_exec("/usr/bin/curl ".escapeshellarg($db2)." -o ".escapeshellarg($csv)." 2>&1");
+if(!file_exists($csv)){
+	echo "An error occured during creaturemodeldata import: ".$outputdump;
+}else{
+	echo "	Writing creaturemodeldata..";
+	$pdo->exec("
+		LOAD DATA LOCAL INFILE '".$csv."'
+		INTO TABLE `wowdata`.creaturemodeldata
+		FIELDS TERMINATED BY ',' ESCAPED BY '\b'
+		LINES TERMINATED BY '\n'
+		IGNORE 1 LINES
+		(@id, @geobox1, @geobox2, @geobox3, @geobox4, @geobox5, @geobox6, @flags, @filedataid) SET id=@id, filedataid=@filedataid
+	");
+	echo "..done!\n";
+}
