aba7687dd0c120145afef5b524cc6956b55c9eaf
Martin Benjamins <ictmartin@gmail.com>
2019-08-30 14:03:18 +0200

    Use new endpoint in install overview

--- builds/extract.php
+++ builds/extract.php
@@ -1,56 +1,8 @@
 <?php
 include("../inc/config.php");
-// include("inc/functions.php");
-ini_set('memory_limit','1G');
 include("../inc/header.php");
 ?>
 <div class="container-fluid">
-	<?php
-	/*
-	$arr = $pdo->query("SELECT ".$_SESSION['product']."_versions.buildconfig, ".$_SESSION['product']."_buildconfig.description FROM ".$_SESSION['product']."_versions LEFT OUTER JOIN ".$_SESSION['product']."_buildconfig ON ".$_SESSION['product']."_versions.buildconfig=".$_SESSION['product']."_buildconfig.hash ORDER BY ".$_SESSION['product']."_buildconfig.description")->fetchAll();
-	<div class="alert alert-info" role="alert">
-		<h4 class="alert-heading">Packages</h4>
-		<p>I'm currently testing a script that compiles all of the install files for a certain build, OS and architecture and packages them up. You can try it out below.</p>
-		<p class="mb-0">
-			<form method='GET' action='/scripts/downloadBuild.php'>
-				<select name='build'>
-					<?php foreach($arr as $row){?>
-					<option value='<?=$row['buildconfig']?>'<? if($row['buildconfig'] == $_GET['build']){ echo " SELECTED"; }?>><?=$row['description']?></option>
-					<?php }?>
-				</select>
-				<select name='os'>
-					<option value='Windows'>Windows</option>
-					<option value='OSX'>OSX</option>
-					<option value='Web'>Web</option>
-				</select>
-				<select name='arch'>
-					<option value='x86_32'>x86_32</option>
-					<option value='x86_64'>x86_64</option>
-				</select>
-				<input type='submit' value='Download'>
-			</form>
-			<br><b>Keep in mind this is highly experimental and there's a good chance that it will time out.</b>
-		</p>
-	</div>
-	<h3>Downloading specific files manually</h3>
-	<p>Select a build to continue.</p>
-	<form action='extract.php' method='GET'>
-		<select name='build'>
-			<?php foreach($arr as $row){?>
-			<option value='<?=$row['buildconfig']?>'<? if($row['buildconfig'] == $_GET['build']){ echo " SELECTED"; }?>><?=$row['description']?></option>
-			<?php }?>
-		</select>
-		<input type='hidden' name='type' value='install'>
-		<input type='submit'>
-	</form>
-
-	*/
-
-	?>
-<!-- <div class="alert alert-info" role="alert">
-		<h4 class="alert-heading">Packages</h4>
-		<p>Packages are currently disabled due to their popularity, they weren't really meant to be used by the general public. Sorry!</p>
-</div> -->
 	<?php
 	if(!empty($_GET['build'])){
 		$build = getVersionByBuildConfigHash($_GET['build']);
@@ -59,11 +11,7 @@ include("../inc/header.php");
 			die("Invalid buildconfig specified!");
 		}
 
-		$output = shell_exec("cd /home/wow/buildbackup; /usr/bin/dotnet BuildBackup.dll dumpinstall wow ".$build['buildconfig']['install_cdn']." | sort");
-
 		echo "<h3>Install dump for ".$build['buildconfig']['description']."</h3>";
-
-		$lines = explode("\n", $output);
 
 		$buildn = substr($build['buildconfig']['description'], 4, 5);
 
@@ -94,7 +42,7 @@ include("../inc/header.php");
 
 		?>
 
-		<table class="table table-sm table-hover table-striped">
+		<table id='install' class="table table-sm table-hover table-striped">
 			<thead class="thead-inverse">
 				<tr>
 					<th>Filename</th>
@@ -107,38 +55,34 @@ include("../inc/header.php");
 				</tr>
 			</thead>
 			<tbody>
-				<?php
-				foreach($lines as $line){
-					if(empty(trim($line))) continue;
-					$split1 = explode(" (", $line);
-					$split2 = explode (", ", $split1[1]);
-					$filename = $split1[0];
-					$size = str_replace("size: ", "", $split2[0]);
-					$md5 = str_replace("md5: ", "", $split2[1]);
-					$tagraw = str_replace("tags: ", "", $split2[2]);
-					$tagraw = explode(",", $tagraw);
-					$tags = array();
-					foreach($tagraw as $tag){
-						$exploded = explode("=", $tag);
-						$tags[$exploded[0]][] = str_replace(")", "", $exploded[1]);
-					}
-
-					echo "<tr>";
-					echo "<td>".$filename."</td>";
-					echo "<td>".$size."</td>";
-					if(!empty($headers)){
-						foreach($headers as $key => $header){
-							echo "<td>".implode(", ", $tags[$key])."</td>";
-						}
-					}
-
-					echo "<td><span class='hash'>".$md5."</span></td>";
-					echo "<td><a href='https://wow.tools/casc/file/chash?contenthash=".$md5."&buildconfig=".$build['buildconfig']['hash']."&cdnconfig=".$build['cdnconfig']['hash']."&filename=".basename(str_replace("\\", "//", $filename))."'>Download</a>";
-					echo "</tr>";
-				}
-				?>
 			</tbody>
 		</table>
+
+		<script type='text/javascript'>
+			$.getJSON("https://wow.tools/casc/install/dump?hash=<?=$build['buildconfig']['install_cdn']?>", function( data ) {
+				var items = [];
+				$.each( data, function( key, val ) {
+					var row = "<tr><td>" + val.name + "</td><td>" + val.size + "</td>";
+
+					var prevTag = 0;
+
+					val.tags.forEach(function(tag) {
+						var splitTag = tag.split('=');
+
+						if(prevTag != splitTag[0]){
+							row += "</td><td>";
+						}
+
+						row += splitTag[1] + " ";
+
+						prevTag = splitTag[0];
+					});
+
+					row += "<td><span class='hash'>" + val.contentHash + "</span></td><td><a href='https://wow.tools/casc/file/chash?contenthash=" + val.contentHash + "&buildconfig=<?=$build['buildconfig']['hash']?>&cdnconfig=<?=$build['cdnconfig']['hash']?>&filename=" + val.name.split('\\').reverse()[0] + "'>Download</a></td></tr>";
+					$('#install tbody').append(row);
+				});
+			});
+		</script>
 		<?php
 	}
 	?>
