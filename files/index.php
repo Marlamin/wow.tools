d9f54df7efe40cf9ad780557348acdfab9bb9b63
Martin Benjamins <marlamin@marlamin.com>
2019-09-25 21:25:43 +0200

    Change Classic Beta listfile download to Classic

--- files/index.php
+++ files/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../inc/header.php");
 $buildq = $pdo->prepare("SELECT hash, description FROM `wow_buildconfig` WHERE product = ? ORDER BY description DESC LIMIT 1;");
-$lfproducts = array("wow", "wowt", "wow_classic_beta");
+$lfproducts = array("wow", "wowt", "wow_classic");
 $lfbuilds = [];
 foreach($lfproducts as $lfproduct){
 	$buildq->execute([$lfproduct]);
