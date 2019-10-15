575cad4f2331d69858c56b63f893dbd250bb6bc0
Martin Benjamins <marlamin@marlamin.com>
2019-10-15 20:26:50 +0200

    Add first detected column to hotfixes page

--- dbc/hotfixes.php
+++ dbc/hotfixes.php
@@ -4,11 +4,11 @@ require_once("../inc/header.php");
 <div class='container-fluid'>
 <table class='table'>
 <thead>
-	<tr><th>Push ID</th><th>Table name</th><th>Record ID</th><th>Build</th></tr>
+	<tr><th>Push ID</th><th>Table name</th><th>Record ID</th><th>Build</th><th>First seen at</th></tr>
 <?php
-foreach($pdo->query("SELECT * FROM wow_hotfixes ORDER BY pushID DESC") as $hotfix){
+foreach($pdo->query("SELECT * FROM wow_hotfixes ORDER BY firstdetected DESC, pushID DESC") as $hotfix){
 ?>
-<tr><td><?=$hotfix['pushID']?></td><td><?=$hotfix['tableName']?></td><td><?=$hotfix['recordID']?></td><td><?=$hotfix['build']?></td></tr>
+<tr><td><?=$hotfix['pushID']?></td><td><?=$hotfix['tableName']?></td><td><?=$hotfix['recordID']?></td><td><?=$hotfix['build']?></td><td><?=$hotfix['firstdetected']?></td></tr>
 <? } ?>
 </table>
 </div>
