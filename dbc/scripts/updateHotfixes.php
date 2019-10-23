120f41c060e433bb02012144baf9124a42bd1c35
Martin Benjamins <marlamin@marlamin.com>
2019-10-23 13:53:51 +0200

    Batch Telegram messages

--- dbc/scripts/updateHotfixes.php
+++ dbc/scripts/updateHotfixes.php
@@ -14,12 +14,22 @@ foreach($files as $file) {
 	$json = json_decode($output, true);
 
 	$insertQ = $pdo->prepare("INSERT IGNORE INTO wow_hotfixes (pushID, recordID, tableName, isValid, build) VALUES (?, ?, ?, ?, ?)");
+	$messages = [];
 	foreach($json['entries'] as $entry){
 		$insertQ->execute([$entry['pushID'], $entry['recordID'], $entry['tableName'], $entry['isValid'], $json['build']]);
 		if($insertQ->rowCount() == 1){
 			echo "Inserted new hotfix: Push ID " . $entry['pushID'] .", Table " . $entry['tableName'] . " ID " .$entry['recordID']." from build " . $json['build']."\n";
-			telegramSendMessage("Inserted new hotfix: Push ID " . $entry['pushID'] .", Table " . $entry['tableName'] . " ID " .$entry['recordID']." from build " . $json['build']);
+
+			if(!array_key_exists($entry['pushID'], $messages)){
+				$messages[$entry['pushID']] = "Push ID " . $entry['pushID'] . " for build " . $json['build']."\n";
+			}
+
+			$messages[$entry['pushID']] .= $entry['tableName'] . " ID " .$entry['recordID']."\n";
 		}
+	}
+
+	foreach($messages as $message){
+		telegramSendMessage($message);
 	}
 
 	$processedMD5s[] = $md5;
