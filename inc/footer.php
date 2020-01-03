435c753a17d93ece63926b1886a53b9d3708382d
Martin Benjamins <marlamin@marlamin.com>
2020-01-03 21:50:44 +0100

    Datamining doc isn't new anymore

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 	<footer>
 	<div class="container text-muted">
-		<p><a href='https://discord.gg/BYpgyhy'>Discord</a> - <a href='https://patreon.com/Marlamin'>Patreon</a> - <a href='/faq.php'>FAQ</a> - <a href='/about.php'>About</a> - <a href='https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/' target='_BLANK'><span class='text-danger'>(New)</span> An introduction to WoW datamining</a></p>
+		<p><a href='https://discord.gg/BYpgyhy'>Discord</a> - <a href='https://patreon.com/Marlamin'>Patreon</a> - <a href='/faq.php'>FAQ</a> - <a href='/about.php'>About</a> - <a href='https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/' target='_BLANK'>An introduction to WoW datamining</a></p>
 		<p>World of Warcraft and related trademarks are registered trademarks of Blizzard Entertainment whom this site is not affiliated with.</p>
 	</div>
 	</footer>
