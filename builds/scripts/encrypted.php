45b6c6927b729533cd5875a3414e10f2ad938f39
Martin Benjamins <marlamin@marlamin.com>
2020-01-12 22:06:11 +0100

    Prefer local endpoint

--- builds/scripts/encrypted.php
+++ builds/scripts/encrypted.php
@@ -69,7 +69,7 @@ echo "[TACT key list] Dumping current TACT keys for ".$fullbuild."..\n";
 
 $inserted = 0;
 
-$db2 = file_get_contents("https://wow.tools/dbc/api/data/tactkeylookup/?build=".$fullbuild."&draw=1&start=0&length=1000");
+$db2 = file_get_contents("http://127.0.0.1:5000/api/data/tactkeylookup/?build=".$fullbuild."&draw=1&start=0&length=1000");
 $tactkeylookups = json_decode($db2, true)['data'];
 echo "[TACT key list] Have " . count($tactkeylookups) ." TACT key lookups from tactkeylookup.db2..\n";
 
@@ -95,7 +95,7 @@ echo "[TACT key list] Done, inserted " . $inserted . " new TACT keys!\n";
 
 $updated = 0;
 
-$db2 = file_get_contents("https://wow.tools/dbc/api/data/tactkey/?build=".$fullbuild."&draw=1&start=0&length=1000");
+$db2 = file_get_contents("http://127.0.0.1:5000/api/data/tactkey/?build=".$fullbuild."&draw=1&start=0&length=1000");
 $tactkeys = json_decode($db2, true)['data'];
 echo "[TACT key list] Have " . count($tactkeys) ." TACT keys from tactkey.db2..\n";
 
