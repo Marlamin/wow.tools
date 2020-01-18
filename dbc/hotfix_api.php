82336f3f776e6d31a91f8a48d08985871b38acc4
Martin Benjamins <marlamin@marlamin.com>
2020-01-18 19:52:05 +0100

    Hotfix diffs

--- dbc/hotfix_api.php
+++ dbc/hotfix_api.php
@@ -2,6 +2,23 @@
 require_once(__DIR__ . "/../inc/config.php");
 header('Content-Type: application/json');
 
+$fullbuilds = $pdo->query("SELECT build, version FROM wow_builds")->fetchAll(PDO::FETCH_KEY_PAIR);
+$buildsToID = $pdo->query("SELECT build, id FROM wow_builds")->fetchAll(PDO::FETCH_KEY_PAIR);
+$tablesToID = $pdo->query("SELECT name, id FROM wow_dbc_tables")->fetchAll(PDO::FETCH_KEY_PAIR);
+
+$versionTableCache = [];
+foreach($pdo->query("SELECT versionid, tableid FROM wow_dbc_table_versions") as $tv){
+	$versionTableCache[$tv['versionid']][] = $tv['tableid'];
+}
+
+function isTableAvailableForBuild($table, $build){
+	global $buildsToID;
+	global $tablesToID;
+	global $versionTableCache;
+
+	return in_array($tablesToID[strtolower($table)], $versionTableCache[$buildsToID[$build]]);
+
+}
 $start = (int)filter_input( INPUT_GET, 'start', FILTER_SANITIZE_NUMBER_INT );
 $length = (int)filter_input( INPUT_GET, 'length', FILTER_SANITIZE_NUMBER_INT );
 
@@ -17,7 +34,7 @@ $dataq->execute();
 
 $returndata['data'] = array();
 while($row = $dataq->fetch()){
-	$returndata['data'][] = array($row['pushID'], $row['tableName'], $row['recordID'], $row['build'], $row['firstdetected']);
+	$returndata['data'][] = array($row['pushID'], $row['tableName'], $row['recordID'], $fullbuilds[$row['build']], $row['firstdetected'], isTableAvailableForBuild($row['tableName'], $row['build']));
 }
 
 echo json_encode($returndata);
--- dbc/hotfixes.php
+++ dbc/hotfixes.php
@@ -4,13 +4,35 @@ require_once(__DIR__ . "/../inc/header.php");
 <div class='container-fluid'>
 	<table class='table' id='hotfixTable'>
 		<thead>
-			<tr><th>Push ID</th><th>Table name</th><th>Record ID</th><th>Build</th><th>First seen at</th></tr>
+			<tr><th>Push ID</th><th>Table name</th><th>Record ID</th><th>Build</th><th>First seen at</th><th>&nbsp;</th></tr>
 		</thead>
 		<tbody>
 
 		</tbody>
 	</table>
 </div>
+<div class="modal" id="hotfixModal" tabindex="-1" role="dialog" aria-labelledby="hotfixModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="hotfixModalLabel">Hotfix diff</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<p>Keep in mind hotfix diffs might be influenced by hotfixes that have since come out as well as not always being up-to-date (depending on <a href='https://wow.tools/uploader.php' target='_NEW'>user uploads</a>).</p>
+			</div>
+			<div class="modal-body" id="hotfixModalContent">
+				<i class="fa fa-refresh fa-spin" style="font-size:24px"></i>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<link href="/dbc/css/dbc.css?v=<?=filemtime(__DIR__ . "/css/dbc.css")?>" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/dataTables.bootstrap4.min.js"></script>
@@ -30,9 +52,65 @@ require_once(__DIR__ . "/../inc/header.php");
 		"ordering": false,
 		"searching": false,
 		"language": { "search": "<a class='btn btn-dark btn-sm btn-outline-primary' href='#' onClick='toggleFilters()' style='margin-right: 10px'>Toggle filters</a> Search: _INPUT_ " },
-		"search": { "search": "" }
+		"search": { "search": "" },
+		"columnDefs": [
+		{
+			"targets": 5,
+			"render": function ( data, type, full, meta ) {
+				if(full[5]){
+					showRowDiff(full[1], full[3], full[2]);
+					return "<div class='resultHolder' id='resultHolder-" + full[1] + "-" + full[3] + "-" + full[2] +"'><i class='fa fa-refresh fa-spin' style='font-size: 12px'></i></div>";
+				}else{
+					return "<i class='fa fa-ban'></i> Not available in client";
+				}
+			}
+		}]
 	});
 
+	function showRowDiff(dbc, build, recordID){
+		var beforeReq = fetch("/dbc/api/peek/" + dbc.toLowerCase() + "?build=" + build + "&col=ID&val=" + recordID + "&useHotfixes=false&calcOffset=false").then(data => data.json());
+		var afterReq = fetch("/dbc/api/peek/" + dbc.toLowerCase() + "?build=" + build + "&col=ID&val=" + recordID + "&useHotfixes=true&calcOffset=false").then(data => data.json());
+
+		Promise.all([beforeReq, afterReq])
+		.then(json => {
+			let before = new Array();
+			json[0].values.map(obj => {
+				before[obj.item1] = obj.item2;
+			});
+
+			let after = new Array();
+			json[1].values.map(obj => {
+				after[obj.item1] = obj.item2;
+			});
+
+			let changes = "<table>";
+			if(Object.keys(before).length == 0){
+				Object.keys(after).forEach(function (key) {
+					changes += "<tr><td>"+ key + "</td><td><ins class='diff-added'>"+after[key] + "</ins></td></tr>";
+				});
+			} else if(Object.keys(after).length == 0){
+				Object.keys(before).forEach(function (key) {
+					changes += "<tr><td>"+ key + "</td><td><del class='diff-removed'>"+before[key] + "</del></td></tr>";
+				});
+			}else{
+				Object.keys(before).forEach(function (key) {
+					if(before[key] != after[key]){
+						changes += "<tr><td>" + key + "</td><td><del class='diff-removed'>" + before[key] + "</del> &rarr; <ins class='diff-added'>" + after[key] + "</ins></td></tr>";
+					}
+				});
+			}
+
+			changes += "</table>";
+
+			if(changes == "<table></table>")
+				changes = "No changes detected";
+
+			var resultHolder = document.getElementById("resultHolder-" + dbc + "-" + build + "-" + recordID);
+			if(resultHolder){
+				resultHolder.innerHTML = changes;
+			}
+		});
+	}
 </script>
 <?php
 require_once(__DIR__ . "/../inc/footer.php");
