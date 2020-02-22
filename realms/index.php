2afb256909ce70cd5589a608526de3793f6fd4d9
Martin Benjamins <marlamin@marlamin.com>
2020-02-22 02:42:46 +0100

    Tabbed realm status (WIP)

--- realms/index.php
+++ realms/index.php
@@ -37,14 +37,47 @@ function getGroupInfo($group){
 	$explid = explode("-", $expl[1]);
 
 	switch($explid[0]){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			$groupInfo['project'] = "Retail";
+			break;
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 25:
+		case 26:
+			$groupInfo['project'] = "Tournament";
+			break;
 		case 41:
+		case 42:
 		case 43:
-			$groupInfo['project'] = "Classic";
+		case 44:
+		case 45:
+			$groupInfo['project'] = "Classic Retail";
+			break;
+		case 50:
+			$groupInfo['project'] = "Public Test Realm";
+			break;
+		case 60:
+			$groupInfo['project'] = "Alpha";
+			break;
+		case 71:
+		case 72:
+		case 73:
+		case 74:
+		case 75:
+		case 76:
+			$groupInfo['project'] = "Dungeon Tournament";
 			break;
 		default:
-			$groupInfo['project'] = "Mainline";
+			$groupInfo['project'] = "Unknown (".$explid[0].")";
 	}
 
+	$groupInfo["id"] = $explid[0];
 	$groupInfo["region"] = strtoupper($expl[0]);
 	$groupInfo["desc"] = $groupInfo["region"] . " - " . $groupInfo['project'];
 	return $groupInfo;
@@ -62,11 +95,28 @@ foreach($pdo->query("SELECT * FROM wow_realms ORDER BY version DESC, name, id AS
 ?>
 <div class="container-fluid">
 <p>Realm status for every known US/EU/TEST realm, updated every 5 minutes. Basic realm status is not retrieved from the Official API allowing for more realms (including PTR/Beta) to be listed as well. Still WIP!</p>
-
 <?php
+?>
+<ul class="nav nav-tabs" role="tablist">
+<?php
+$firstNav = true;
 foreach($groupedRealms as $region => $realms){
 ?>
-<h1 id="<?=$region?>"><?=$region?></h1>
+<li class="nav-item">
+	<a class="nav-link <?php if($firstNav){?>active<?} $firstNav = false; ?>" data-toggle="tab" href="#group<?=$realms[key($realms)]['group']['id']?>" role="tab"><?=$region?></a>
+</li>
+<?php
+}
+?>
+</ul>
+<div class="tab-content">
+<?php
+
+$firstTab = true;
+foreach($groupedRealms as $region => $realms){
+?>
+<div class="tab-pane <?php if($firstTab){?>show active<?php $firstTab = false; } ?>" id="group<?=$realms[key($realms)]['group']['id']?>" role="tabpanel">
+<h1><?=$region?></h1>
 <table class='table table-sm table-striped'>
 <thead><tr><th style='width: 80px'></th><th style='width: 300px'>Name</th><th style='width: 50px;'>Type</th><th style='width: 80px'>Population</th><th>Server version</th><th>Last seen in realmlist at (UTC+1)</th></tr></thead>
 <?php
@@ -76,7 +126,7 @@ foreach($groupedRealms as $region => $realms){
 	}
 ?>
 </table>
-
+</div>
 <?php
 }
 ?>
