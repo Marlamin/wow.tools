c4a30bbdca7d6f4f25d81eadf20059e5eb2cb04a
Martin Benjamins <marlamin@marlamin.com>
2020-03-15 15:04:11 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 	<footer>
 	<div class="container text-muted">
-		<p><a href='https://discord.gg/BYpgyhy'>Join our Discord</a> - <a href='https://patreon.com/Marlamin'>Patreon</a>/<a href='https://paypal.me/Marlamin'>Donate</a> - <a href='/faq.php'>FAQ</a> - <a href='/about.php'>About</a><br><a href='https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/' target='_BLANK'>Introduction to WoW datamining</a> - <a href='https://strawpoll.com/48kgd7ck' target='_BLANK'><span class='text-danger'>Vote</span></a> <a href='https://www.patreon.com/posts/33285749'>on WoW.tools todo list</a></p>
-		<p style='font-size: 11px'>World of Warcraft and related trademarks are registered trademarks of Blizzard Entertainment whom this site is not affiliated with.</p>
+		<p><a href='https://discord.gg/BYpgyhy'>Join our Discord</a> - <a href='https://patreon.com/Marlamin'>Patreon</a>/<a href='https://paypal.me/Marlamin'>Donate</a> - <a href='/faq.php'>FAQ</a> - <a href='/about.php'>About</a><br><a href='https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/' target='_BLANK'>Introduction to WoW datamining</a> - <a href='https://www.patreon.com/marlamin/posts' target='_BLANK'>Monthly WoW.tools update posts</a></p>
+		<p style='font-size: 11px'>World of Warcraft and related trademarks are registered trademarks of Blizzard Entertainment whom this site is not affiliated with. With ♥ by <a href='https://twitter.com/marlamin' target='_BLANK'>Marlamin</a>.</p>
 	</div>
 	</footer>
 </body>
