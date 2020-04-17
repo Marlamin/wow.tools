65435832f19f5470ffa532156d65685e8233146b
Martin Benjamins <marlamin@marlamin.com>
2020-04-17 13:07:08 +0200

    Handle all fields named "Flags" as flags

--- dbc/index.php
+++ dbc/index.php
@@ -397,7 +397,7 @@ $dbFound = false;
 								}else{
 									returnVar = "<a style='padding-top: 0px; padding-bottom: 0px; cursor: pointer; border-bottom: 1px dotted;' data-toggle='modal' data-target='#fkModal' onclick='openFKModal(" + full[meta.col] + ", \"" + fkCols[meta.col] + "\", \"" + $("#buildFilter").val() + "\")'>" + full[meta.col] + "</a>";
 								}
-							}else if(flagMap.has(currentParams["dbc"] + '.' + json["headers"][meta.col])){
+							}else if(json["headers"][meta.col] == "Flags" || flagMap.has(currentParams["dbc"] + '.' + json["headers"][meta.col])){
 								returnVar = "<span style='padding-top: 0px; padding-bottom: 0px; cursor: help; border-bottom: 1px dotted;' data-trigger='hover' data-container='body' data-html='true' data-toggle='popover' data-content='" + getFlagDescriptions(currentParams["dbc"], json["headers"][meta.col], full[meta.col]).join(",<br> ") + "'>0x" + Number(full[meta.col]).toString(16) + "</span>";
 							}else if(enumMap.has(currentParams["dbc"] + '.' + json["headers"][meta.col])){
 								returnVar = full[meta.col] + " <i>(" + getEnum(vars["dbc"].toLowerCase(), json["headers"][meta.col], full[meta.col]) + ")</i>";
