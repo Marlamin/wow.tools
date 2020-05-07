28959a03f9e33606a44ce64d0f27d1d5f169b682
Martin Benjamins <marlamin@marlamin.com>
2020-05-07 22:01:19 +0200

    Quick landing page for DB

---
+++ db/index.php
@@ -0,0 +1,8 @@
+<?php require_once("../inc/header.php"); ?>
+<div class='container-fluid'>
+<h2>WDB cache info dumps</h2>
+<p>
+<a href='/db/creatures.php'>View creatures</a> - <a href='/db/quests.php'>View quests</a>
+</p>
+</div>
+<?php require_once("../inc/footer.php"); ?>
