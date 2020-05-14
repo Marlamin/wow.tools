2519dd44c5e185a9938c0ee423ce27709d3f095c
Martin Benjamins <marlamin@marlamin.com>
2020-05-14 22:38:58 +0200

    Implement fullrun options for WDB reading

--- dbc/scripts/updateCaches.php
+++ dbc/scripts/updateCaches.php
@@ -2,11 +2,17 @@
 if(php_sapi_name() != "cli") die("This script cannot be run outside of CLI.");
 require_once(__DIR__ . "/../../inc/config.php");
 
+$fullrun = false;
+if(!empty($argv[1]) && $argv[1] == "fullrun"){
+	echo "Full run, reimporting all caches!\n";
+	$fullrun = true;
+}
+
 $processedMD5s = [];
 $files = glob('/home/wow/dbcdumphost/caches/*.wdb');
 foreach($files as $file) {
 	// Only process hotfixes newer than 6 hours ago
-	if(filemtime($file) < strtotime("-6 hours"))
+	if(!$fullrun && filemtime($file) < strtotime("-6 hours"))
 		continue;
 
 	$md5 = md5_file($file);
@@ -15,7 +21,7 @@ foreach($files as $file) {
 
 	echo "[Cache updater] Reading " . $file . "\n";
 	$output = shell_exec("cd /home/wow/wdbupdater; dotnet WoWTools.WDBUpdater.dll " . escapeshellarg($file) . " mysql");
-	if(substr($output, -34) != "New entries: 0\nUpdated entries: 0\n"){
+	if(!$fullrun && substr($output, -34) != "New entries: 0\nUpdated entries: 0\n"){
 		telegramSendMessage($output);
 	}
 	$processedMD5s[] = $md5;
