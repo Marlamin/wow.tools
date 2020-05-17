b582e631de4abb4cbce93bcd79520fe966dd1a00
Martin Benjamins <marlamin@marlamin.com>
2020-05-17 14:28:00 +0200

    Properly signed hex displays

--- dbc/js/dbc.js
+++ dbc/js/dbc.js
@@ -114,23 +114,8 @@ function openFKModal(value, location, build){
 	});
 }
 
-function dec2hex(str){ // .toString(16) only works up to 2^53
-	if(str == '0'){
-		return '0';
-	}
-	var dec = str.toString().split(''), sum = [], hex = [], i, s
-	while(dec.length){
-		s = 1 * dec.shift()
-		for(i = 0; s || i < sum.length; i++){
-			s += (sum[i] || 0) * 10
-			sum[i] = s % 16
-			s = (s - sum[i]) / 16
-		}
-	}
-	while(sum.length){
-		hex.push(sum.pop().toString(16))
-	}
-	return hex.join('')
+function dec2hex(str){
+	return (parseInt(str) >>> 0).toString(16);
 }
 
 function getFlagDescriptions(db, field, value, targetFlags = 0){
