5bcf37300979c853af0f05adc346e442b9005f7a
Martin Benjamins <marlamin@marlamin.com>
2020-09-08 23:15:02 +0200

    Drop filename history to 7 days to stop crashing browsers

--- files/submitQueue.php
+++ files/submitQueue.php
@@ -133,11 +133,11 @@ if(!empty($_SESSION['loggedin']) && $_SESSION['rank'] > 0){
 			?>
 		</table>
 	<?php } ?>
-	<h3>Filenames submitted in the last 30 days</h3>
+	<h3>Filenames submitted in the last 7 days</h3>
 	<table class='table table-striped table-condensed'>
 			<?php
 			$previousTime = '';
-			$filenames = $pdo->query("SELECT * FROM wow_rootfiles_suggestions WHERE submitted BETWEEN NOW() - INTERVAL 30 DAY AND NOW() ORDER BY submitted DESC")->fetchAll();
+			$filenames = $pdo->query("SELECT * FROM wow_rootfiles_suggestions WHERE submitted BETWEEN NOW() - INTERVAL 7 DAY AND NOW() ORDER BY submitted DESC")->fetchAll();
 			if(count($filenames) > 0){
 				echo "<thead><tr><th style='width: 100px'>User</th><th style='width: 100px'>Status</th><th style='width: 200px'>Submitted at</th><th>Files</th></tr></thead>";
 				foreach($filenames as $row){
