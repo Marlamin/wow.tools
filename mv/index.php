6d3796576566dd4604da76b355f151b6dc3148b4
Martin Benjamins <marlamin@marlamin.com>
2021-02-04 15:41:21 +0100

    Add portal culling checkbox

--- mv/index.php
+++ mv/index.php
@@ -116,7 +116,8 @@ b.fillRect(d,m,n,p);b.fillStyle=l;b.globalAlpha=.9;b.fillRect(d,m,n,p);return{do
                     <input type='checkbox' id='retailOnly' name='settings[retailOnly]'> <label for='retailOnly'>Use static files (fastest, limited to current retail build)</label><br>
                     <input type='color' id='customClearColor' name='settings[customClearColor]'> <label for='customClearColor'>Background color (applied on next model load)</label><br>
                     <input type='text' id='farClip' name='settings[farClip]'> <label for='farClip'>View distance</label><br>
-                    <input type='text' id='farClipCull' name='settings[farClipCull]'> <label for='farClipCull'>Model culling distance</label>
+                    <input type='text' id='farClipCull' name='settings[farClipCull]'> <label for='farClipCull'>Model culling distance</label><br>
+                    <input type='checkbox' id='portalCulling' name='settings[portalCulling]'> <label for='portalCulling'>Enable portal culling?</label>
                 </form>
             </div>
             <div class="modal-footer">
