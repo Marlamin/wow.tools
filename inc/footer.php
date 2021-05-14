9de6e38a11a32f71315d25cab7a45d268f31ba5e
LordMagnum <45822783+LordMagnum@users.noreply.github.com>
2021-05-14 19:08:58 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,17 @@
-	<footer>
-	<div class="container text-muted">
-		<p><a href='https://discord.gg/BYpgyhy'>Join our Discord</a> - <a href='/faq.php'>FAQ</a> - <a href='/about.php'>About</a><br><a href='https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/' target='_BLANK'>Introduction to WoW datamining</a> - <a class='text-warning' href='/uploader/'>Contribute data to WoW.tools</a></p>
-		<p style='font-size: 11px'>World of Warcraft and related trademarks are registered trademarks of Blizzard Entertainment whom this site is not affiliated with.</p>
-	</div>
-	</footer>
-</body>
+		<footer>
+			<div class="container text-muted">
+				<p>
+					<a href="https://discord.gg/BYpgyhy">Join our Discord</a> - 
+					<a href="/faq.php">FAQ</a> - 
+					<a href="/about.php">About</a><br />
+					<a href="https://docs.google.com/document/d/1y1fHaZ1PrLvUTNM8crz081b4t8_pXjOYPxnagc-Y94c/" target="_BLANK">Introduction to WoW datamining</a> - 
+					<a class="text-warning" href="/uploader/">Contribute data to WoW.tools</a>
+				</p>
+
+				<p style="font-size: 11px">
+					World of Warcraft and related trademarks are registered trademarks of Blizzard Entertainment whom this site is not affiliated with.
+				</p>
+			</div>
+		</footer>
+	</body>
 </html>
