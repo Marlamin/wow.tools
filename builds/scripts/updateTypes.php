f89a77b0bf902617a72b10312441f13a794cc228
Martin Benjamins <marlamin@marlamin.com>
2021-06-26 14:40:03 +0200

    Use regular BuildBackup for type guesser

--- builds/scripts/updateTypes.php
+++ builds/scripts/updateTypes.php
@@ -130,7 +130,7 @@ while (true) {
         echo("Extracting " . $toextract . " unknown files (" . $extracted . " already extracted) for buildconfig " . $buildconfig . "\n");
         // echo "cd /home/wow/buildbackup; /usr/bin/dotnet /home/wow/buildbackup/BuildBackup.dll extractfilesbylist ".$buildconfig." ".$cdnrow['cdnconfig']." /tmp/casc/".$buildconfig."/ /tmp/casc/".$buildconfig.".txt";
         if ($toextract > 0) {
-            $cmd = "cd /home/wow/buildbackup2; /usr/bin/dotnet /home/wow/buildbackup2/BuildBackup.dll extractfilesbylist " . $buildconfig . " " . $cdnrow['cdnconfig'] . " /tmp/casc/" . $buildconfig . "/ /tmp/casc/" . $buildconfig . ".txt";
+            $cmd = "cd /home/wow/buildbackup; /usr/bin/dotnet /home/wow/buildbackup/BuildBackup.dll extractfilesbylist " . $buildconfig . " " . $cdnrow['cdnconfig'] . " /tmp/casc/" . $buildconfig . "/ /tmp/casc/" . $buildconfig . ".txt";
             exec($cmd, $output);
         }
 
